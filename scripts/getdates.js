cf016412c9037514ae7a5b1026fdab12a4527700
GMarinP <giovannamarinpereira@gmail.com>
2026-01-10 20:02:29 +0100

    finished

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+console.log(document.lastModified)
+document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
