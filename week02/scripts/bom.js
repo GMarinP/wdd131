809b0f26ed9984e9a48cedf2363b00fd2c8a0982
GMarinP <giovannamarinpereira@gmail.com>
2026-01-14 11:02:56 +0100

    some more w2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,8 +4,27 @@ const list = document.querySelector('______');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
 
+
+button.addEventListener('click', function() {
+    if (input.value.trim() != ''){
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        return
+    }
+    else {
+         alert('Please enter a chapter first');
+        input.focus();   
+        return; 
+    }
+});
+
+deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    input.focus();
+});
+
+input.value = '';
+input.focus();
